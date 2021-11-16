f6d762187600e778fce2fbb976f93563c8ce1865
alena <obdolbannaya@mail.ru>
2021-11-16 13:02:12 +0300

    добавлен тест на отправку письма

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -9,7 +9,13 @@ public class MainPage extends BasicPage{
     private String passwordFieldXpath = "//*[@type=\"password\"]";
     private String enterPassButtonXpath = "//*[@data-testid=\"enter-password\"]";
     private String enterButtonXpath = "//*[@data-testid=\"login-to-mail\"]";
-    //данные
+    //адреса для отправки нового письма
+    private String createNewLetter = "//span[text()='Написать письмо']";
+    private String receiverNewLetter = "//input[contains(@class,'container')]";
+    private String topicNewLetter = "//input[@name='Subject']";
+    private String textNewLetter = "//div[@role='textbox']";
+    private String sendNewLetter = "//span[text()='Отправить']";
+    // данные
     private String login = "enotov.enot@internet.ru";
     private String password = "pass51423";
 
@@ -23,5 +29,17 @@ public class MainPage extends BasicPage{
         clickOnElement(By.xpath(enterPassButtonXpath));
         sendKeys(By.xpath(passwordFieldXpath),password);
         clickOnElement(By.xpath(enterButtonXpath));
+    }
+
+    @Step("Отправляем письмо на адрес {address}")
+    public void sendLetter(String address){
+        open();
+        login();
+        clickOnElement(By.xpath(createNewLetter));
+        sendKeys(By.xpath(receiverNewLetter),address);
+        sendKeys(By.xpath(topicNewLetter),address);
+        sendKeys(By.xpath(topicNewLetter),address);
+        sendKeys(By.xpath(textNewLetter),address);
+        clickOnElement(By.xpath(sendNewLetter));
     }
 }
--- src/test/java/apiTests/LettersApiTest.java
+++ src/test/java/apiTests/LettersApiTest.java
@@ -3,14 +3,18 @@ package apiTests;
 import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pages.MainPage;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LettersApiTest extends BaseApiTest{
-    private final static String HASH = "851d352505ada46a8c7d92dc9a83be1a";
+public class LettersApiTest extends BaseApiTest {
+    private final static String HASH = "e0c9d02ddb72226c384758097db0045d";
     private static String key;
+    private static String address;
 
     @Step("Создаем почту")
     @BeforeAll
@@ -24,6 +28,7 @@ public class LettersApiTest extends BaseApiTest{
                 .when().log().all().get("/api.php")
                 .then().log().all().statusCode(200);
         key = response.extract().response().jsonPath().get("key");
+        address = response.extract().response().jsonPath().get("email");
     }
 
     @Step("Получаем входящие письма")
@@ -35,5 +40,40 @@ public class LettersApiTest extends BaseApiTest{
                 .queryParam("key", key)
                 .when().log().all().get("/api.php")
                 .then().log().all().statusCode(200);
+    }
+
+
+    @Step("Получаем входящие письма после отправки нового письма")
+    @Test
+    public void receiveNewLetterTest() {
+        MainPage page = new MainPage();
+        page.sendLetter(address);
+        //письмо отправляется не сразу, поэтому приходится ждать
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ValidatableResponse listResponse = given()
+                .queryParam("action", "getlist")
+                .queryParam("hash", HASH)
+                .queryParam("key", key)
+                .when().log().all().get("/api.php")
+                .then().log().all().statusCode(200);
+        int id = listResponse.extract().response().jsonPath().getInt("[0].id");
+        ValidatableResponse response = given()
+                .queryParam("action", "getmail")
+                .queryParam("hash", HASH)
+                .queryParam("key", key)
+                .queryParam("id", id)
+                .when().log().all().get("/api.php")
+                .then().log().all().statusCode(200);
+        assertTrue(response.extract().response().jsonPath().get("message").toString().contains(address));
+    }
+
+    @AfterAll
+    public static void close() {
+        MainPage page = new MainPage();
+        page.close();
     }
 }
